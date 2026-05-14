b8e6c2cc01432726e5ad3c1eab9829cf48a72f62
Harshad Inarkar <harshad.inarkar@gmail.com>
2026-05-14 23:28:55 +0530

    pushing latest changes

--- analysis/tradeapi/start_ssh_proxy.sh
+++
@@ -1,6 +0,0 @@
-echo "Kill SSH Process"
-pkill -f "ssh -i ~/.ssh/oracle_dhan.key"
-kill -15 $(lsof -t -i:9090)
-
-echo "Start Ssh Proxy"
-ssh -i ~/.ssh/oracle_dhan.key -D 9090 ubuntu@80.225.247.216 -N -f
