d623ee6e08c12aedd7d4f64efd61ef2d0334c670
Harshad Inarkar <harshad.inarkar@gmail.com>
2026-06-10 12:16:23 +0530

    pushing latest changes

--- analysis/tradeapi/dhan_trade.py
+++ analysis/tradeapi/dhan_trade.py
@@ -102,7 +102,7 @@ def _format_expiry_time(expiry_time: str) -> str:
         expiry_time = dt.strftime("%Y-%m-%d  %H:%M")
 
     except (ValueError, TypeError) as e:
-        LOGGER.info("Date conversion failed %s %s", expiry_time, e)
+        LOGGER.info("Date conversion failed %s\n%s", expiry_time, e)
 
     return expiry_time
 
