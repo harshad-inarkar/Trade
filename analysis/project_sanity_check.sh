2c1ea8c71ce8e4de6d562acd938731501699fc6b
Harshad Inarkar <harshad.inarkar@gmail.com>
2026-06-16 20:21:49 +0530

    pushing latest changes

--- analysis/project_sanity_check.sh
+++ analysis/project_sanity_check.sh
@@ -3,13 +3,22 @@
 # You can pass directories to ruff directly, and it will recursively check all files in them.
 # If you want to include both specific files and entire directories, simply include them in the list.
 
+# targets=(
+#     web_scripts/trade_client/trade_app.py
+#     tradeapi/dhan_trade.py
+#     tradeapi/scrip_master.py
+#     tradeapi/scrip_search.py
+#     utils/network
+# )
+
 targets=(
     web_scripts/trade_client/trade_app.py
-    tradeapi/dhan_trade.py
-    tradeapi/scrip_master.py
-    tradeapi/scrip_search.py
-    utils/network
+    tradeapi
+    utils
+    tradeview
+    orchest
 )
+
 
 ruff check --fix "${targets[@]}"
 ruff format "${targets[@]}"
